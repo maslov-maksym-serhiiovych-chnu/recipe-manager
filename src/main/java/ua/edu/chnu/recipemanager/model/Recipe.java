53dca918a20183ff9f46ebb80ff578f1292c61d4
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2024-06-28 19:26:39 +0300

    generate models

---
+++ src/main/java/ua/edu/chnu/recipemanager/model/Recipe.java
@@ -0,0 +1,28 @@
+package ua.edu.chnu.recipemanager.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "recipes")
+public class Recipe {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Size(max = 255)
+    @NotNull
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @NotNull
+    @Lob
+    @Column(name = "description", nullable = false)
+    private String description;
+}
