fb4f55c08da9423de8166ba05ed3b03d0cf04228
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2024-06-29 07:45:49 +0300

    update repository

--- src/main/java/ua/edu/chnu/recipemanager/repository/RecipeRepository.java
+++ src/main/java/ua/edu/chnu/recipemanager/repository/RecipeRepository.java
@@ -1,9 +1,9 @@
 package ua.edu.chnu.recipemanager.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import ua.edu.chnu.recipemanager.model.Recipe;
 
 @Repository
-public interface RecipeRepository extends JpaRepository<Recipe, Integer> {
+public interface RecipeRepository extends CrudRepository<Recipe, Integer> {
 }
