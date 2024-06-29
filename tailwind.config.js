1560992009687d2917644d33ea4f57a14664769d
maslov-maksym-serhiiovych-chnu <maslov.maksym@chnu.edu.ua>
2024-06-29 05:15:16 +0300

    build front-end

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
     content: ['src/main/resources/templates/**/*.html'],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sans: ['Roboto', 'sans-serif'],
+            }
+        },
     },
     plugins: [],
 }
